aab9830ca9a6daf2096fdc75a7835877cab3400c
German Vargas <germandavidvargas@hotmail.com>
2023-07-28 18:50:28 -0500

    feat: serializers for mongodb

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,10 +10,16 @@ builder.Services.AddControllers(options =>
 {
     //Don't removes the Async suffix from the action names
     options.SuppressAsyncSuffixInActionNames = false;
-});
+})
+;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Serialize mongodb Ids as strings
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+//Serialize mongodb DateTimes as strings
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
 var app = builder.Build();
 
